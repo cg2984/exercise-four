b7c8df53932adab26dc809bc3c763726bad0c6f7
Clara Gilligan <31659760+cg2984@users.noreply.github.com>
2020-04-01 09:54:40 -0400

    adding variable port

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const express = require('express');
 
 const app = express();
 //we need to change the port to be a variable so that heroku can set it for us
-//if there is a port val then do that or (if that isnt there) do 4000
+//if there is a port val then do that or (if that isnt there) use port 4000
 let port = process.env.PORT || 4000;
 
 //the function recieves two arguements, the path and the arrow function
 //req = request res = response
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello Clara, I am Sentient!'))
 
 //function is express object 
 //the two arguements are the port you have and a console log
