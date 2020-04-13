05fe68324bf3b1af55d2ead8de01483d1f04c3ca
Clara Gilligan <31659760+cg2984@users.noreply.github.com>
2020-04-13 12:53:42 -0400

    messing around

--- routes/createArticle.js
+++ routes/createArticle.js
@@ -11,7 +11,7 @@ const db = firebase.firestore();
 const blogposts = db.collection("blogposts");
 
 //making form
-const form = `<form> 
+const form = `<form action='/create/submit'> 
 	<input type="text" name="title" placeholder="Title"/>
 	<input type="text" name="author" placeholder="Author"/>
 	<input type="text" name="text" placeholder="Text"/>
@@ -24,12 +24,17 @@ router.get("/", (req,res) => res.send(form));
 //submits the form
 router.get("/submit", (req,res) => {
 	const queryParams = req.query;
+	//a regular expression or regex
+	//removes the spaces from the new posts that you submit and replaces it with dashes
+	const idFromTitle = queryParams.title.replace(/\s+/g,
+		"-").toLowerCase();
 
 	blogposts
-		.doc()
+		//this makes the id in the database the title of the post
+		.doc(idFromTitle)
 		.set(queryParams)
 		.then(function(doc){
-			res.send("<h1> Submission Success</h1><p>a href='/create>' Create another Post</a></p>");
+			res.send("<h1> Submission Success</h1><p><a href='/create'>Create another Post</a></p><p><a href='/'>Home</a></p>");
 		})
 		.catch(function(error){
 			console.log("error", error);
